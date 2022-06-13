ce6875ab772699b090a35a158f1c52c9d8a102fd
面向搜索引擎编程 <mapno1.com@gmail.com>
2022-06-13 22:09:18 +0800

    Update start.sh

--- bin/start.sh
+++ bin/start.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 #cd "$(dirname "$0")"
-cd /mnt/us/extensions/OutlookCalendarWeather/bin/
+cd /mnt/us/extensions/KindleOutlookCalendarWeather/bin/
 mntroot rw
 #nohup python3 -u WeatherStation.py > log.log 2>&1 &
 
 #python3 WeatherStation.py
-fbink -c -g file=/mnt/us/extensions/OutlookCalendarWeather/bin/black.png,w=600,halign=center,valign=center
+fbink -c -g file=/mnt/us/extensions/KindleOutlookCalendarWeather/bin/black.png,w=600,halign=center,valign=center
 sleep 1
 fbink -c
-fbink -c -g file=/mnt/us/extensions/OutlookCalendarWeather/bin/logo.png,w=600,halign=center,valign=center
+fbink -c -g file=/mnt/us/extensions/KindleOutlookCalendarWeather/bin/logo.png,w=600,halign=center,valign=center
 nohup python3 -u WeatherStation.py > log.log 2>&1 &
